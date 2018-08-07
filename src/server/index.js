db64df631fb300445a48de7b1d4da34e91ddb872
673800357 <673800357@qq.com>
2018-08-07 20:07:34 +0800

    fix 异步函数问题

--- src/server/index.js
+++ src/server/index.js
@@ -1,70 +1,73 @@
-const Res = require('./lib/res');
+const Res = require("./lib/res");
 class TcbRouter {
-	constructor({ event, defaultRes, callback }) {
-		if (!callback || typeof callback !== 'function') {
-			throw new Error('Callback must be a function');
-		}
-		this._middlewares = [];
-		const old_callback = callback;
-		if (defaultRes) {
-			callback = (err, data) => {
-				if (err) {
-					if (err.message) {
-						err = err.message;
-					}
-					return old_callback(null, new Res({ message: err, code: 1 }));
-				} else {
-					return old_callback(null, new Res({ data, message: 'ok' }));
-				}
-			};
-		}
-		this._req = { event, url: event.url };
-		this._res = { callback };
-	}
-	receive(path, handle) {
-		// 这个东西是否需要
-		if (typeof handle !== 'function') {
-			handle = path;
-			path = '*';
-		}
-		this._middlewares.push({ handle, path, method: 'receive' });
-	}
-	use(path, handle) {
-		if (typeof handle !== 'function') {
-			handle = path;
-			path = '*';
-		}
-		this._middlewares.push({ handle, path, method: 'middleware' });
-	}
-	apply() {
-		// 相当于http来了
-		let index = 0;
-		const next = err => {
-			if (index === this._middlewares.length) {
-				this._res.callback('无法匹配');
-				return;
-			} else if (err) {
-				this._res.callback(err);
-			} else {
-				const { handle, method, path } = this._middlewares[index++];
-				if (
-					path === '*' ||
-					new RegExp(`^${this._req.url}$`).test(path) // 暂时只精准匹配
-				) {
-					if (method === 'middleware') {
-						// 中间件
-						handle(this._req, this._res, next);
-					} else {
-						// 路由
-						handle(this._req, this._res);
-					}
-				} else {
-					index++;
-					next();
-				}
-			}
-		};
-		next();
-	}
+    constructor({ event, defaultRes, callback }) {
+        if (!callback || typeof callback !== "function") {
+            throw new Error("Callback must be a function");
+        }
+        this._middlewares = [];
+        const old_callback = callback;
+        if (defaultRes) {
+            callback = (err, data) => {
+                if (err) {
+                    if (err.message) {
+                        err = err.message;
+                    }
+                    return old_callback(
+                        null,
+                        new Res({ message: err, code: 1 })
+                    );
+                } else {
+                    return old_callback(null, new Res({ data, message: "ok" }));
+                }
+            };
+        }
+        this._req = { event, url: event.url };
+        this._res = { callback };
+    }
+    receive(path, handle) {
+        // 这个东西是否需要
+        if (typeof handle !== "function") {
+            handle = path;
+            path = "*";
+        }
+        this._middlewares.push({ handle, path, method: "receive" });
+    }
+    use(path, handle) {
+        if (typeof handle !== "function") {
+            handle = path;
+            path = "*";
+        }
+        this._middlewares.push({ handle, path, method: "middleware" });
+    }
+    apply() {
+        // 相当于http来了
+        let index = 0;
+
+        let goNext = true;
+
+        const makeNextTrue = () => {
+            console.log("调用next");
+            goNext = true;
+        };
+        // 该用while
+        while (index < this._middlewares.length && goNext) {
+            const { handle, method, path } = this._middlewares[index++];
+            if (
+                path === "*" ||
+                new RegExp(`^${this._req.url}$`).test(path) // 暂时只精准匹配
+            ) {
+                goNext = false;
+                if (method === "middleware") {
+                    // 中间件
+                    console.log("进入中间件");
+                    handle(this._req, this._res, makeNextTrue);
+                } else {
+                    // 路由
+                    console.log("本次调用路由路径", path);
+                    return handle(this._req, this._res);
+                }
+            }
+        }
+    }
 }
 module.exports = TcbRouter;
